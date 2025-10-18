89f09b0325e49493cadf60f6b8187310807e363f
xwilson03 <xanderwilson2014@gmail.com>
2025-10-18 17:04:49 -0500

    updates gravity, adds fastfall

--- Player.gd
+++ Player.gd
@@ -2,19 +2,23 @@ extends CharacterBody2D
 
 
 @export var SPEED: float = 250.
-@export var JUMP_VELOCITY: float = 400.0
+@export var JUMP_VELOCITY: float = 500.0
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float):
-    
-    # Apply gravity to velocity if airborne
-    if not is_on_floor():
-        velocity.y += gravity * delta
 
     # Handle jump
     if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y -= JUMP_VELOCITY
+        velocity.y = -JUMP_VELOCITY
+    
+    # Handle fastfall
+    if Input.is_action_just_released("jump") and velocity.y < 0:
+        velocity.y = 0
+    
+    # Handle gravity
+    if not is_on_floor():
+        velocity.y += gravity * delta
 
     # Handle movement
     var direction_x: float = Input.get_axis("left", "right")
